90ee8f6fd71b008feb49602cc5b2d587c8cc8f73
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2024-10-14 16:21:35 +0300

    fix style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Add the class "scrolled" to the header element when scrolling more than 50 pixels on the page.
+// Добавляем класс «scrolled» в элемент заголовка при прокрутке страницы более чем на 50 пикселей.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -10,6 +11,7 @@ function scrollFunction() {
 }
 
 // Add the class "open" to the header element when the burger menu is opened.
+// Добавляем класс «open» в элемент заголовка, когда открывается бургер-меню.
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("burger").addEventListener("click", function (){
         document.querySelector("header").classList.toggle("open");
@@ -22,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Добавляем кнопку header при 768 в меню бургер
 function moveButton() {
     const button = document.querySelector('.header__button')
     const menuList = document.querySelector('.menu__list')
@@ -36,78 +39,43 @@ function moveButton() {
 moveButton()
 window.addEventListener('resize', moveButton)
 
+
 // Add the class "lock" to the body element to disable scrolling.
+// Добавьте класс «lock» в элемент body, чтобы отключить прокрутку.
 const header = document.querySelector('header');
 const body = document.querySelector('body');
 
-function addLockClass() {
-    body.classList.add('lock');
-}
-
-function removeLockClass() {
-    body.classList.remove('lock');
-}
-
-function checkMenuState() {
+function toggleLockClass() {
     if (header.classList.contains('open')) {
-        addLockClass();
+        body.classList.add('lock');
     } else {
-        removeLockClass();
+        body.classList.remove('lock');
     }
 }
 
-document.addEventListener('DOMContentLoaded', checkMenuState);
-header.addEventListener('click', checkMenuState);
+document.addEventListener('DOMContentLoaded', toggleLockClass);
+header.addEventListener('click', toggleLockClass);
+
+
 
 
 // Take into account the height of the header when scrolling to the necessary blocks.
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.querySelectorAll('.menu__link').forEach(anchor => {
-//         anchor.addEventListener('click', function (e) {
-//             e.preventDefault();
-//
-//             const targetId = this.getAttribute('href').substring(1);
-//             const targetElement = document.getElementById(targetId);
-//
-//             if (targetElement) {
-//                 const headerHeight = document.getElementById('header').offsetHeight;
-//                 const targetPosition = targetElement.offsetTop - headerHeight;
-//
-//                 window.scrollTo({
-//                     top: targetPosition,
-//                 });
-//             }
-//         });
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.menu__link, .button-anchor').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const burgerBtn = document.getElementById("burger");
-//     const header = document.querySelector("header");
-//
-//     // Открытие/закрытие бургер-меню
-//     burgerBtn.addEventListener("click", function () {
-//         header.classList.toggle("open");
-//     });
-//
-//     document.querySelectorAll(".menu__link").forEach(function (link) {
-//         link.addEventListener("click", function (e) {
-//             e.preventDefault();
-//
-//             // Прокрутка к нужной секции
-//             const targetId = this.getAttribute("href").substring(1);
-//             const targetElement = document.getElementById(targetId);
-//
-//             if (targetElement) {
-//                 const headerHeight = header.offsetHeight;
-//                 const targetPosition = targetElement.offsetTop - headerHeight;
-//
-//                 // Закрытие бургер-меню
-//                 header.classList.remove("open");
-//
-//                 // Прокрутка к целевому элементу
-//                 window.scrollTo(0, targetPosition);
-//             }
-//         });
-//     });
-// });
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const headerHeight = document.getElementById('header').offsetHeight;
+                const targetPosition = targetElement.offsetTop - headerHeight;
+
+                window.scrollTo({
+                    top: targetPosition,
+                });
+            }
+        });
+    });
+});
